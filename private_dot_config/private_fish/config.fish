2e07fea3e975f96c0bd8bf65e8e864c063fcac7a
joshuakraitberg <joshuakraitberg@users.noreply.github.com>
2025-03-23 20:21:57 -0400

    Enable atuin

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -U fish_greeting ""
 
+    # Activate atuin
+    atuin init fish | source
+
     # Reload aliases
     source ~/.config/fish/conf.d/aliases.fish
 end
