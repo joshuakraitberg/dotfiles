bf2a34452ed090741f5114fd6867cedf7b1b7ca3
joshuakraitberg <joshuakraitberg@users.noreply.github.com>
2025-09-21 21:22:59 -0400

    Improved bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,23 +4,30 @@ set -e
 
 DOTFILES_REPO="github.com/joshuakraitberg/dotfiles"
 
-# Do nothing if chezmoi data is present already
-[ ! -e ~/.config/chezmoi/chezmoi.yaml ] || {
-  echo "chezmoi is already configured"
-  exit 0
+which() {
+  sh -c -- "which $1 >/dev/null 2>&1"
 }
 
-# Install required packages
-if which pacman; then
-  sudo pacman -S --noconfirm chezmoi
-elif which snap; then
-  sudo snap install chezmoi --classic
-elif which apt-get; then
-  (cd /tmp && sh -c "$(curl -fsLS get.chezmoi.io)" && sudo mv bin/chezmoi /usr/local/bin/)
-else
-  echo 'Unknown package manager, cannot install chezmoi'
-  exit 1
+if ! which chezmoi; then
+  echo "Installing chezmoi..."
+
+  if which pacman; then
+    sudo pacman -S --noconfirm chezmoi
+  elif which snap; then
+    sudo snap install chezmoi --classic
+  elif which apt-get; then
+    (cd /tmp && sh -c "$(curl -fsLS get.chezmoi.io)" && sudo mv bin/chezmoi /usr/local/bin/)
+  else
+    echo 'Unknown package manager, cannot install chezmoi'
+    exit 1
+  fi
 fi
 
-# Init and deploy
-chezmoi init "${DOTFILES_REPO}" --apply -v
+# Only init first time
+if [ ! -e ~/.config/chezmoi/chezmoi.yaml ]; then
+  echo "Initializing chezmoi..."
+  chezmoi init "${DOTFILES_REPO}" -v
+fi
+
+echo "Applying dotfiles..."
+chezmoi apply -v
