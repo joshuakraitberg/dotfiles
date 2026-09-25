489d56d2306a00ee041583432b05bdb066a889a4
joshuakraitberg <joshuakraitberg@users.noreply.github.com>
2026-09-25 17:27:48 -0400

    Updates

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -1,3 +1,7 @@
+if test -r ~/.config/fish/secrets.fish
+    source ~/.config/fish/secrets.fish
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -U fish_greeting ""
@@ -6,14 +10,23 @@ if status is-interactive
     set -xg VISUAL nvim
     set -xg EDITOR nvim
 
+    # Set SUDO_ASKPASS: zenity by default, fall back to ksshaskpass
+    if set -q SUDO_ASKPASS
+        # already set, leave it
+    else if command -v zenity &>/dev/null
+        set -xg SUDO_ASKPASS ~/.local/bin/sudo-askpass
+    else if command -v ksshaskpass &>/dev/null
+        set -xg SUDO_ASKPASS (command -v ksshaskpass)
+    end
+
+    # Activate bun
+    fish_add_path ~/.bun/bin
+
     # Activate atuin (filter out deprecated bind -k syntax)
     atuin init fish | string replace -r '^\s*bind -M insert -k up.*$' '' | source
 
     # Activate zoxide
     zoxide init fish | source
-
-    # Add bun
-    fish_add_path -a $HOME/.bun/bin
 
     # Reload aliases
     source ~/.config/fish/conf.d/aliases.fish
